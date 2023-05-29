9455a0eda8a410f166684c347cb951ac08222b23
Selvarajan Thangavel <selvarajan2006@gmail.com>
2023-05-29 22:54:34 +0530

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Selvarajan on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
